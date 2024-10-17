04da0627a48c6371ddaa6bc856e38bc49c85df77
Jekov-Evgen <jena1245481te@gmail.com>
2024-10-17 21:47:02 +0300

    Добавлена возможность чтения файлов по главному ключу

--- reading_data_from_a_file.py
+++ reading_data_from_a_file.py
@@ -1,1 +1,16 @@
-class Read
+import json
+
+class Read:
+    def __init__(self) -> None:
+        pass
+    
+    def read_connection_key(self, connection_key):
+        with open("bd.json", "r") as fl:
+            temp = fl.read()
+            transformation = json.loads(temp)
+            result = transformation.get(str(connection_key))
+            
+            return result
+        
+tt = Read()        
+print(tt.read_connection_key(1))
