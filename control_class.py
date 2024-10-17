7de0410e784d8a88de0ec1a9cec6c27dae951673
Jekov-Evgen <jena1245481te@gmail.com>
2024-10-17 22:28:54 +0300

    .

--- control_class.py
+++ control_class.py
@@ -1,3 +1,9 @@
+from writing_data_to_a_file import Write
+from reading_data_from_a_file import Read
+from data_change import Editing
+
 class Control:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, connection_key : int, key_to_meaning, meaning) -> None:
+        self.write = Write(connection_key, key_to_meaning, meaning)
+        self.read = Read()
+        self.editing = Editing()
